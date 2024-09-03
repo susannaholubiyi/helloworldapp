11968d196e1fec7658cbe6ecb0398b2464a1af08
susannaholubiyi <susannaholubiyi@gmail.com>
2024-09-03 11:02:45 +0100

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM  maven:3.8.7 as build
+COPY .. .
+RUN mvn -B clean package -DskipTests
+
+FROM openjdk:17
+COPY --from=build target/*.jar signInApp.jar
+
+ENTRYPOINT ["java", "-jar", "-Dserver.port=8080", "helloWorldApp.jar"]
+
---
+++ src/test/java/com/semicolon/helloworldapp/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.helloworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
